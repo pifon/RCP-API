271800febf653b176c33134ec1be5cd3e092d2ca
Przemyslaw Wolski <przemek.wolski@gmail.com>
2024-12-16 14:52:07 +0000

    Start developing API

--- routes/api.php
+++ routes/api.php
@@ -4,16 +4,16 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+// Welcome, ping route
 Route::get('/', function (){
     return response()->json([
-        "message" => "Hello API!",
+        "message" => sprintf("Welcome to %s API.", ucfirst(config('app.name'))),
         200
     ]);
 });
 
 Route::post('/login',[AuthController::class, 'login']);
 Route::post('/register',[AuthController::class, 'register']);
-// Route::post('/register', 'AuthController@register');
 
 Route::get('/user', function (Request $request) {
     return $request->user();
