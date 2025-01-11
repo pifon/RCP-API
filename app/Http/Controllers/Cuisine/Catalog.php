576c67f0da1dd74c4e6824691687b5ad738f8894
Przemyslaw Wolski <przemek.wolski@gmail.com>
2025-01-11 22:45:23 +0000

    Added quality tools and checks

--- app/Http/Controllers/Cuisine/Catalog.php
+++ app/Http/Controllers/Cuisine/Catalog.php
@@ -4,28 +4,23 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers\Cuisine;
 
-use App\Entities\Cuisine;
 use App\Http\Controllers\Controller;
 use App\Repositories\CuisineRepository;
 use App\Transformers\CuisineTransformer;
 use Illuminate\Http\Request;
 
-
 class Catalog extends Controller
 {
     public function __construct(
         private readonly CuisineRepository $repository,
-        private readonly CuisineTransformer  $transformer
-    ) {
-    }
+        private readonly CuisineTransformer $transformer
+    ) {}
 
     /**
-     * @param Request $request
      * @return array<string, mixed>
      */
     public function __invoke(Request $request): array
     {
-
         $cuisines = $this->repository->getCuisines();
 
         return $this->transformer->transformSet($cuisines);
